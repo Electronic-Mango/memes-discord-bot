b4aa68e92e3251a265038a878ef8dd4bbfa4edbc
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-05 19:14:43 +0200

    Log image url and size in the "get" command

--- src/bot/command/get.py
+++ src/bot/command/get.py
@@ -4,6 +4,8 @@ Command Cog sending back a random meme.
 
 from aiohttp import ClientSession
 from io import BytesIO
+from logging import getLogger
+from sys import getsizeof
 
 from discord import File
 from discord.ext.commands import Cog, Context, command
@@ -12,13 +14,16 @@ from image_getter import get_random_image_url
 
 
 class Get(Cog, name="Get a random meme"):
+    def __init__(self) -> None:
+        self._logger = getLogger(__name__)
+
     @command(name="get")
     async def get(self, context: Context) -> None:
         """Get a random meme"""
         meme_url = get_random_image_url()
         image_bytes = await self._load_image(meme_url)
+        self._logger.info(f"Trying to send [{meme_url}] [{getsizeof(image_bytes)}] bytes")
         await context.send(file=File(image_bytes, meme_url))
-    
 
     async def _load_image(self, url: str) -> bytes:
         async with ClientSession() as session:
