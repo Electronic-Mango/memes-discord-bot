52554ac4461d70d8672af089b4e61be74e3688a8
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-05 21:58:07 +0200

    Allow configuring "pasta" command language via dedicated command
    Allow configuring to which language "pasta" command output will be translated via "setlanguage" command.

--- src/bot/command/pasta.py
+++ src/bot/command/pasta.py
@@ -1,5 +1,6 @@
 """
 Command Cog sending back a random pasta.
+Output language can be configured with dedicated command.
 """
 
 from os import getenv
@@ -10,22 +11,32 @@ from dotenv import load_dotenv
 from more_itertools import sliced
 
 from pasta_getter import get_pasta
-from translator import translate
+from translator import is_valid_language, translate
 
 load_dotenv
 _MAX_TEXT_MESSAGE_LENGTH = int(getenv("MAX_TEXT_MESSAGE_LENGTH"))
-_TARGET_LANGUAGE = getenv("TARGET_LANGUAGE")
 
 
 class Pasta(Cog, name="Get a random pasta"):
     def __init__(self) -> None:
         self._logger = getLogger(__name__)
+        self._language = None
 
     @command(name="pasta")
     async def pasta(self, context: Context) -> None:
         """Get a random pasta"""
         pasta = get_pasta()
-        pasta = translate(pasta, _TARGET_LANGUAGE)
+        if self._language:
+            pasta = translate(pasta, self._language)
         sliced_pasta = sliced(pasta, _MAX_TEXT_MESSAGE_LENGTH)
         for slice in sliced_pasta:
             await context.reply(slice)
+    
+    @command(name="setlanguage", aliases=["lang", "language"])
+    async def set_language(self, context: Context, *, target_language: str) -> None:
+        """Set language for "pasta" command output"""
+        if not is_valid_language(target_language):
+            await context.reply(f"{target_language} is not valid")
+        else:
+            self._language = target_language
+            await context.reply(f"Set language to **{target_language}**")
--- src/translator.py
+++ src/translator.py
@@ -1,7 +1,11 @@
-from googletrans import Translator
+from googletrans import LANGUAGES, Translator
 
 _translator = Translator()
 
 
 def translate(source: str, target_language: str) -> str:
     return _translator.translate(text=source, dest=target_language).text
+
+
+def is_valid_language(language: str) -> bool:
+    return language in LANGUAGES or language in LANGUAGES.values()
