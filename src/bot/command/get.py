e70d4aa1d8d06e32f86b342c5abb2f72effecad1
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-05 19:38:32 +0200

    Skip too large (>8MB) files in "get" command

--- src/bot/command/get.py
+++ src/bot/command/get.py
@@ -2,6 +2,7 @@
 Command Cog sending back a random meme.
 """
 
+from os import getenv
 from aiohttp import ClientSession
 from io import BytesIO
 from logging import getLogger
@@ -12,6 +13,8 @@ from discord.ext.commands import Cog, Context, command
 
 from image_getter import get_random_image_url
 
+MAX_FILESIZE_BYTES = int(getenv("MAX_FILESIZE_BYTES"))
+
 
 class Get(Cog, name="Get a random meme"):
     def __init__(self) -> None:
@@ -20,12 +23,19 @@ class Get(Cog, name="Get a random meme"):
     @command(name="get")
     async def get(self, context: Context) -> None:
         """Get a random meme"""
-        meme_url = get_random_image_url()
-        image_bytes = await self._load_image(meme_url)
-        self._logger.info(f"Trying to send [{meme_url}] [{getsizeof(image_bytes)}] bytes")
-        await context.send(file=File(image_bytes, meme_url))
+        media, url = await self._get_media()
+        while getsizeof(media) > MAX_FILESIZE_BYTES:
+            self._logger.info(f"Too large [{url}], max is [{MAX_FILESIZE_BYTES}], trying again")
+            media, url = await self._get_media()
+        await context.reply(file=File(media, url))
 
-    async def _load_image(self, url: str) -> bytes:
+    async def _get_media(self) -> tuple[bytes, str]:
+        meme_url = get_random_image_url()
+        media_bytes = await self._download_media(meme_url)
+        self._logger.info(f"Trying to send [{meme_url}] [{getsizeof(media_bytes)}] bytes")
+        return media_bytes, meme_url
+
+    async def _download_media(self, url: str) -> bytes:
         async with ClientSession() as session:
             async with session.get(url) as response:
                 return BytesIO(await response.read())
