d5b8e442aaf864ffd826e29cc7c2fd6e0a077e38
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-05 20:26:44 +0200

    Allow specifying multiple media sources via YAML file
    By default "sources.yml" file in project root will be used.
    Path to this file can be overriden via "MEDIA_SOURCES_FILE" environment variable or ".env".

--- src/image_getter.py
+++ src/image_getter.py
@@ -1,19 +1,32 @@
+from logging import getLogger
 from os import getenv
 from random import choice
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from feedparser import parse
+from yaml import safe_load
 
 load_dotenv()
-IMAGES_RSS_FEED = getenv("IMAGES_RSS_FEED")
+_MEDIA_SOURCES_FILE = getenv("MEDIA_SOURCES_FILE")
+
+_logger = getLogger(__name__)
 
 
 def get_random_image_url() -> str:
-    feed = parse(IMAGES_RSS_FEED)
+    feed_source = _get_random_source()
+    feed = parse(feed_source)
     feed_items = list(feed.entries)
     random_item = choice(feed_items)
     media_source = BeautifulSoup(random_item.summary, "html.parser")
     media_elements = media_source.find_all("img")
     media_sources = [media["src"] for media in media_elements]
     return choice(media_sources)
+
+
+def _get_random_source() -> str:
+    with open(_MEDIA_SOURCES_FILE) as sources_file:
+        sources = safe_load(sources_file)
+    source_name, source_url = choice(list(sources.items()))
+    _logger.info(f"Using [{source_name}] [{source_url}]")
+    return source_url
