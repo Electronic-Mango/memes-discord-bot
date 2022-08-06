aaa8051bfa204a926e7a49b3fce79f4d86c3876f
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-06 21:51:50 +0200

    Extract pasta JSON key to ".env"

--- src/pasta_getter.py
+++ src/pasta_getter.py
@@ -5,8 +5,9 @@ from requests import get
 
 load_dotenv()
 _PASTA_API_SOURCE = getenv("PASTA_API_SOURCE")
+_PASTA_API_TEXT_KEY = getenv("PASTA_API_TEXT_KEY")
 
 
 def get_pasta() -> str:
     item = get(_PASTA_API_SOURCE).json()
-    return item["selftext"].strip()
+    return item[_PASTA_API_TEXT_KEY].strip()
