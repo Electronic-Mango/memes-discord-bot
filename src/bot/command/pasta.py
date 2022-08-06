b569e9db4a719068a6fb61c42df1d0ee901ec288
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-06 22:31:53 +0200

    Store pasta language per channel, rather than globally

--- src/bot/command/pasta.py
+++ src/bot/command/pasta.py
@@ -22,14 +22,14 @@ _DEEP_FRIED_LANGUAGE = getenv("DEEP_FRIED_LANGUAGE")
 class Pasta(Cog, name="Get a random pasta"):
     def __init__(self) -> None:
         self._logger = getLogger(__name__)
-        self._language = None
+        self._languages = dict()
 
     @command(name="pasta")
     async def pasta(self, context: Context) -> None:
         """Get a random pasta"""
         pasta = get_pasta()
-        if self._language:
-            pasta = translate(pasta, self._language)
+        if context.channel.id in self._languages:
+            pasta = translate(pasta, self._languages[context.channel.id])
         await self._send_pasta(context, pasta)
 
     @command(name="setlanguage", aliases=["lang", "language"])
@@ -38,13 +38,13 @@ class Pasta(Cog, name="Get a random pasta"):
         if not is_valid_language(target_language):
             await context.reply(f"{target_language} is not valid")
         else:
-            self._language = target_language
+            self._languages[context.channel.id] = target_language
             await context.reply(f"Set language to **{target_language}**")
 
     @command(name="resetlanguage", aliases=["resetlang"])
     async def reset_language(self, context: Context) -> None:
         """Reset language for "pasta" command output"""
-        self._language = None
+        self._languages.pop(context.channel.id, None)
         await context.reply("Set language to default")
 
     @command(name="deepfriedpasta", aliases=["dfpasta", "dfp"])
@@ -53,7 +53,7 @@ class Pasta(Cog, name="Get a random pasta"):
         pasta = get_pasta()
         original_language = detect_language(pasta)
         pasta = translate(pasta, _DEEP_FRIED_LANGUAGE)
-        target_language = self._language if self._language else original_language
+        target_language = self._languages.get(context.channel.id, original_language)
         pasta = translate(pasta, target_language)
         await self._send_pasta(context, pasta)
 
