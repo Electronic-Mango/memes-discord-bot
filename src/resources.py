2c0247515259d1f3598cc6ac786f6ae382891903
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-10 21:46:19 +0200

    Allow specifying custom request headers in "sources.yml"

--- src/resources.py
+++ src/resources.py
@@ -25,16 +25,23 @@ def get_pasta() -> str:
     return _get_resource(_TEXT_SOURCES)
 
 
-def _get_resource(sources: list[dict[str, str]]) -> str:
+def _get_resource(sources: list[dict[str, Any]]) -> str:
     source = choice(sources)
     response_json = _load_resource_json(source)
     return _extract_resource_from_json(response_json, source["keys"])
 
 
-def _load_resource_json(source: dict[str, str]) -> dict[str, Any]:
+def _load_resource_json(source: dict[str, Any]) -> dict[str, Any]:
     source_url = source["url"]
-    _logger.info(f"Using [{source_url}]")
-    return get(source_url).json()
+    source_headers = _parse_headers(source)
+    _logger.info(f"Using URL=[{source_url}] headers={source_headers}")
+    return get(source_url, headers=source_headers).json()
+
+
+def _parse_headers(source: dict[str, Any]) -> dict[str, str]:
+    headers = source.get("headers", [])
+    return {header["name"]: header["value"] for header in headers}
+        
 
 
 def _extract_resource_from_json(json_resource: Any, keys: list[str]) -> str:
