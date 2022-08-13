cd51a7765c23d66908165c13fc3545dee03126cb
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-13 23:09:55 +0200

    Migrate to Pycord
    Migrate from discord.py to Pycord.
    Migrate all commands to slash commands.

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -4,20 +4,20 @@ Module creating the bot, adding all required Cogs and running it.
 
 from discord.ext.commands import Bot, when_mentioned_or
 
-from bot.command.media import Media
-from bot.command.text import Text
+from bot.command.media import media_command_group
+from bot.command.text import text_command_group
 from bot.event.on_command import OnCommand
 from bot.event.on_connect import OnConnect
 from bot.event.on_ready import OnReady
-from settings import BOT_COMMAND_PREFIX, BOT_TOKEN
+from settings import BOT_TOKEN
 
 
 def run_bot() -> None:
     """Create, configure and run the bot"""
-    bot = Bot(command_prefix=when_mentioned_or(BOT_COMMAND_PREFIX))
+    bot = Bot()
     bot.add_cog(OnConnect(bot))
     bot.add_cog(OnReady(bot))
     bot.add_cog(OnCommand())
-    bot.add_cog(Media())
-    bot.add_cog(Text())
+    bot.add_application_command(media_command_group)
+    bot.add_application_command(text_command_group)
     bot.run(BOT_TOKEN)
--- src/bot/command/media.py
+++ src/bot/command/media.py
@@ -7,35 +7,41 @@ from logging import getLogger
 from io import BytesIO
 from sys import getsizeof
 
-from discord import File
-from discord.ext.commands import Cog, Context, command
+from discord import ApplicationContext, File, SlashCommandGroup
 
 from resources import get_random_media_url
 from settings import BOT_COMMANDS, BOT_MAX_FILESIZE_BYTES
 
-_MEDIA_COMMAND_NAMES = BOT_COMMANDS["media"]
+_COMMAND = BOT_COMMANDS["media"]
+_COMMAND_GROUP_NAME = _COMMAND["group"]["group_name"]
+_COMMAND_DESCRIPTION = _COMMAND["group"]["description"]
+_GET_COMMAND = _COMMAND["commands"]["get"]
+
+_logger = getLogger(__name__)
+
+media_command_group = SlashCommandGroup(_COMMAND_GROUP_NAME, _COMMAND_DESCRIPTION)
+_command = media_command_group.command
 
 
-class Media(Cog, name="Send back a random media (image, GIFs, videos, etc.)"):
-    def __init__(self) -> None:
-        self._logger = getLogger(__name__)
+@_command(name=_GET_COMMAND["name"], description=_GET_COMMAND["description"])
+async def get_media(context: ApplicationContext) -> None:
+    """Get a random media"""
+    await context.defer()
+    media, url = await _get_media()
+    while getsizeof(media) > BOT_MAX_FILESIZE_BYTES:
+        _logger.info(f"[{url}] [{getsizeof(media)}] exceeds [{BOT_MAX_FILESIZE_BYTES}]")
+        media, url = await _get_media()
+    await context.respond(file=File(media, url.split("/")[-1]))
 
-    @command(name=_MEDIA_COMMAND_NAMES[0], aliases=_MEDIA_COMMAND_NAMES[1:])
-    async def media(self, context: Context) -> None:
-        """Get a random media"""
-        media, url = await self._get_media()
-        while getsizeof(media) > BOT_MAX_FILESIZE_BYTES:
-            self._logger.info(f"[{url}] [{getsizeof(media)}] exceeds [{BOT_MAX_FILESIZE_BYTES}]")
-            media, url = await self._get_media()
-        await context.send(file=File(media, url.split("/")[-1]))
 
-    async def _get_media(self) -> tuple[bytes, str]:
-        media_url = get_random_media_url()
-        media_bytes = await self._download_media(media_url)
-        self._logger.info(f"Trying to send [{media_url}] [{getsizeof(media_bytes)}] bytes")
-        return media_bytes, media_url
+async def _get_media() -> tuple[bytes, str]:
+    media_url = get_random_media_url()
+    media_bytes = await _download_media(media_url)
+    _logger.info(f"Trying to send [{media_url}] [{getsizeof(media_bytes)}] bytes")
+    return media_bytes, media_url
 
-    async def _download_media(self, url: str) -> bytes:
-        async with ClientSession() as session:
-            async with session.get(url) as response:
-                return BytesIO(await response.read())
+
+async def _download_media(url: str) -> bytes:
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            return BytesIO(await response.read())
