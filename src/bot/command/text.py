d8bb22c76a0248ca5b5301c4a27d83cbcf17c682
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-14 14:38:13 +0200

    Move language related commands to a separate command subgroup

--- src/bot/command/text.py
+++ src/bot/command/text.py
@@ -14,21 +14,29 @@ from resources import get_random_text
 from settings import BOT_COMMANDS, BOT_DEEP_FRIED_LANGUAGES, BOT_MAX_TEXT_MESSAGE_LENGTH
 from translator import is_valid_language, SUPPORTED_LANGUAGES, translate
 
-_COMMAND = BOT_COMMANDS["text"]
-_COMMAND_GROUP_NAME = _COMMAND["group"]["group_name"]
-_COMMAND_DESCRIPTION = _COMMAND["group"].get("description")
-_GET_COMMAND = _COMMAND["commands"]["get"]
-_SET_LANGUAGE = _COMMAND["commands"]["set_language"]
-_RESET_LANGUAGE = _COMMAND["commands"]["reset_language"]
-_DEEP_FRY_TEXT = _COMMAND["commands"]["deep_fry_text"]
+_TEXT_GROUP = BOT_COMMANDS["text_group"]
+_TEXT_GROUP_NAME = _TEXT_GROUP["name"]
+_TEXT_GROUP_DESCRIPTION = _TEXT_GROUP.get("description")
+_GET = _TEXT_GROUP["commands"]["get"]
+_DEEP_FRY_TEXT = _TEXT_GROUP["commands"]["deep_fry_text"]
+
+_LANGUAGE_SUBGROUP = _TEXT_GROUP["language_subgroup"]
+_LANGUAGE_SUBGROUP_NAME = _LANGUAGE_SUBGROUP["name"]
+_LANGUAGE_SUBGROUP_DESCRIPTION = _LANGUAGE_SUBGROUP.get("description")
+_SET_LANGUAGE = _LANGUAGE_SUBGROUP["commands"]["set"]
+_RESET_LANGUAGE = _LANGUAGE_SUBGROUP["commands"]["reset"]
 
 _languages = dict()
 
-text_command_group = SlashCommandGroup(_COMMAND_GROUP_NAME, _COMMAND_DESCRIPTION)
-_command = text_command_group.command
+text_command_group = SlashCommandGroup(_TEXT_GROUP_NAME, _TEXT_GROUP_DESCRIPTION)
+_text_command = text_command_group.command
+_language_command_subgroup = text_command_group.create_subgroup(
+    _LANGUAGE_SUBGROUP_NAME, _LANGUAGE_SUBGROUP_DESCRIPTION
+)
+_lang_command = _language_command_subgroup.command
 
 
-@_command(name=_GET_COMMAND.get("name"), description=_GET_COMMAND.get("description"))
+@_text_command(name=_GET.get("name"), description=_GET.get("description"))
 async def text(context: ApplicationContext) -> None:
     """Get a random text message"""
     await context.defer()
@@ -38,31 +46,7 @@ async def text(context: ApplicationContext) -> None:
     await _send_text(context, text)
 
 
-async def _get_languages(context: AutocompleteContext) -> list[str]:
-    input = context.value.lower()
-    return [language for language in SUPPORTED_LANGUAGES if language.startswith(input)]
-
-
-# TODO Perhaps "language" parameter could also be taken from "settings.yml"?
-@_command(name=_SET_LANGUAGE.get("name"), description=_SET_LANGUAGE.get("description"))
-@option("language", description=_SET_LANGUAGE["autocomplete_hint"], autocomplete=_get_languages)
-async def set_language(context: ApplicationContext, *, language: str) -> None:
-    """Set language for text-based commands output"""
-    if is_valid_language(language):
-        _languages[context.channel.id] = language
-        await context.respond(f"Set language to **{language}**")
-    else:
-        await context.respond(f"**{language}** isn't a valid language")
-
-
-@_command(name=_RESET_LANGUAGE.get("name"), description=_RESET_LANGUAGE.get("description"))
-async def reset_language(context: ApplicationContext) -> None:
-    """Reset language for text-based commands output"""
-    _languages.pop(context.channel.id, None)
-    await context.respond("Set language to default")
-
-
-@_command(name=_DEEP_FRY_TEXT.get("name"), description=_DEEP_FRY_TEXT.get("description"))
+@_text_command(name=_DEEP_FRY_TEXT.get("name"), description=_DEEP_FRY_TEXT.get("description"))
 async def deep_fried_text(context: ApplicationContext) -> None:
     """Get a random deep-fried text"""
     await context.defer()
@@ -76,3 +60,26 @@ async def _send_text(context: ApplicationContext, text: str) -> None:
     sliced_text = sliced(escape_markdown(text), BOT_MAX_TEXT_MESSAGE_LENGTH)
     for slice in sliced_text:
         await context.respond(slice.strip())
+
+
+async def _get_languages(context: AutocompleteContext) -> list[str]:
+    input = context.value.lower()
+    return [language for language in SUPPORTED_LANGUAGES if language.startswith(input)]
+
+
+@_lang_command(name=_SET_LANGUAGE.get("name"), description=_SET_LANGUAGE.get("description"))
+@option("language", description=_SET_LANGUAGE.get("autocomplete_hint"), autocomplete=_get_languages)
+async def set_language(context: ApplicationContext, *, language: str) -> None:
+    """Set language for text-based commands output"""
+    if is_valid_language(language):
+        _languages[context.channel.id] = language
+        await context.respond(f"Set language to **{language}**")
+    else:
+        await context.respond(f"**{language}** isn't a valid language")
+
+
+@_lang_command(name=_RESET_LANGUAGE.get("name"), description=_RESET_LANGUAGE.get("description"))
+async def reset_language(context: ApplicationContext) -> None:
+    """Reset language for text-based commands output"""
+    _languages.pop(context.channel.id, None)
+    await context.respond("Set language to default")
