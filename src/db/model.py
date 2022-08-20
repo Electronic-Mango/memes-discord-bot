c23e913509bda8d0e84896fbcedb5a4aaff8a9ec
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-20 19:31:40 +0200

    Add __str__ to DB language model

--- src/db/model.py
+++ src/db/model.py
@@ -4,3 +4,6 @@ class LanguageModel:
     def __init__(self, channel_id: str, language: str) -> None:
         self.channel_id = channel_id
         self.language = language
+
+    def __str__(self) -> str:
+        return f"{self.channel_id} {self.language}"
