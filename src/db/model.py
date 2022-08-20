beb66f6b6cc2c42ff4f7ce2f9e6e2ee8c7ec54e9
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-20 14:44:44 +0200

    Fix db model module formatting

--- src/db/model.py
+++ src/db/model.py
@@ -1,5 +1,6 @@
 class LanguageModel:
     """DB model used for storing languages per Discord channel"""
+
     def __init__(self, channel_id: str, language: str) -> None:
         self.channel_id = channel_id
         self.language = language
