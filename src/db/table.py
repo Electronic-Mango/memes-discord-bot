5acc0a14576a1bcc4f0d7e099be18ed2e8307038
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-20 14:34:45 +0200

    Add missing docstrings for modules in "db" subdirectory

--- src/db/table.py
+++ src/db/table.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, MetaData, String, Table
 
 from settings import LANGUAGES_DB_TABLE_NAME
 
+"""DB table used for storing languages per Discord channel."""
 languages_table = Table(
     LANGUAGES_DB_TABLE_NAME,
     MetaData(),
--- src/db/wrapper.py
+++ src/db/wrapper.py
@@ -1,3 +1,7 @@
+"""
+Wrapper for SQLite DB, used for storing languages per Discord channel.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import mapper, sessionmaker
 
@@ -10,23 +14,27 @@ _session = sessionmaker(_engine)
 
 
 def initialize_database() -> None:
+    """Initialize DB, map model with table, create table if necessary"""
     mapper(LanguageModel, languages_table)
     languages_table.create(_engine, checkfirst=True)
 
 
 def store_language(channel_id: int, language: str) -> None:
+    """Store text language to Discord channel ID mapping"""
     with _session() as session:
         session.merge(LanguageModel(channel_id, language))
         session.commit()
 
 
 def remove_language(channel_id: int) -> None:
+    """Remove stored language for a given Discord channel ID"""
     with _session() as session:
         session.query(LanguageModel).filter(LanguageModel.channel_id == channel_id).delete()
         session.commit()
 
 
 def get_language(channel_id: int) -> str:
+    """Get stored language for a given Discord channel ID, or None if absent"""
     with _session() as session:
         model = session.query(LanguageModel).filter(LanguageModel.channel_id == channel_id).first()
         return model.language if model else None
